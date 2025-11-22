0beb960b8ddf5da93df82359391a210e0965f289
NimeshHB <nimesh105c2@gmail.com>
2025-11-22 14:01:58 +0530

    style: who-we-are  add dark background

--- components/who-we-are.tsx
+++ components/who-we-are.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function WhoWeAre(): JSX.Element {
   return (
-    <section aria-labelledby="who-we-are" className="mt-20 text-white">
+    <section aria-labelledby="who-we-are" className="mt-20 text-white bg-[#071018]">
       <div className="container mx-auto px-6 py-12">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center">
           <div className="lg:col-span-6 order-2 lg:order-1">
